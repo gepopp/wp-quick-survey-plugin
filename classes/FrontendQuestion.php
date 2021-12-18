a8932f2f366406dfc9d082c84f44e2c94e7165c7
Gerhard Popp <gerhrad@poppgerhard.at>
2021-12-18 05:14:20 +0100

    d

--- classes/FrontendQuestion.php
+++ classes/FrontendQuestion.php
@@ -162,15 +162,16 @@ class FrontendQuestion {
                 <div class="p-5 <?php echo ( $is_frontpage ) ? 'bg-white' : 'h-full flex flex-col' ?>">
 
 
-<!--        			--><?php //if ( $is_frontpage ): ?>
+        			<?php if ( $is_frontpage ): ?>
                         <h3 class="text-xl font-semibold text-center mb-5"><?php echo get_the_title( $attached['survey'] ) ?></h3>
                     <?php         var_dump(memory_get_peak_usage()); ?>
-<!--                        <div class="flex justify-center">-->
-<!--                            <div class="flex space-x-10 items-center mb-5 pb-5 border-b border-gray-800">-->
-<!--        						--><?php //if ( has_post_thumbnail( $attached['survey'] ) ): ?>
-<!--                                    <div class="rounded-full flex-none border border-primary-100 w-24 h-24">-->
-<!--        								--><?php //echo get_the_post_thumbnail( $attached['survey'], 'thumbnail', [ 'class' => 'p-2 rounded-full' ] ); ?>
-<!--                                    </div>-->
+                        <div class="flex justify-center">
+                            <div class="flex space-x-10 items-center mb-5 pb-5 border-b border-gray-800">
+        						<?php if ( has_post_thumbnail( $attached['survey'] ) ): ?>
+                                    <div class="rounded-full flex-none border border-primary-100 w-24 h-24">
+        								<?php echo get_the_post_thumbnail( $attached['survey'], 'thumbnail', [ 'class' => 'p-2 rounded-full' ] ); ?>
+                                    </div>
+                    <?php         var_dump(memory_get_peak_usage()); ?>
 <!--        						--><?php //endif; ?>
 <!--                                <div>-->
 <!--                                    <p>-->
