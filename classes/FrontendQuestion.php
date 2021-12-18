0471188a8474122e3c02ec54eddd7efb22c3d62a
Gerhard Popp <gerhrad@poppgerhard.at>
2021-12-18 12:26:25 +0100

    debug

--- classes/FrontendQuestion.php
+++ classes/FrontendQuestion.php
@@ -153,11 +153,9 @@ class FrontendQuestion {
 		ob_start();
 
 		?>
-<!--        <div class="p-5 --><?php //echo ( ! $is_frontpage ) ? 'bg-white' : 'h-full flex flex-col' ?><!--">-->
-<!---->
-<!---->
-<!--			--><?php //if ( ! $is_frontpage ): ?>
-<!--                <h3 class="text-xl font-semibold text-center mb-5">--><?php //echo get_the_title( $attached['survey'] ) ?><!--</h3>-->
+        <div class="p-5 <?php echo ( ! $is_frontpage ) ? 'bg-white' : 'h-full flex flex-col' ?>">
+			<?php if ( ! $is_frontpage ): ?>
+                <h3 class="text-xl font-semibold text-center mb-5"><?php echo $title ?></h3>
 <!--                <div class="flex justify-center">-->
 <!--                    <div class="flex space-x-10 items-center mb-5 pb-5 border-b border-gray-800">-->
 <!--						--><?php //if ( has_post_thumbnail( $attached['survey'] ) ): ?>
@@ -172,7 +170,7 @@ class FrontendQuestion {
 <!--                        </div>-->
 <!--                    </div>-->
 <!--                </div>-->
-<!--			--><?php //endif; ?>
+			<?php endif; ?>
 <!---->
 <!--            <questions-->
 <!--                    :questions="--><?php //echo htmlentities( json_encode( $survey_meta['questions'] ) ) ?><!--"-->
@@ -206,7 +204,7 @@ class FrontendQuestion {
 <!--                    </div>-->
 <!--				--><?php //endif; ?>
 <!--            </div>-->
-<!--        </div>-->
+        </div>
 		<?php
 		$content .= ob_get_clean();
 
