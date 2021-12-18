c4c46173a98e21f1235e664415d078cd8faa9a2d
Gerhard Popp <gerhrad@poppgerhard.at>
2021-12-18 04:24:01 +0100

    wip

--- classes/FrontendQuestion.php
+++ classes/FrontendQuestion.php
@@ -142,26 +142,26 @@ class FrontendQuestion {
 		}
 
 
-//		ob_start();
-//		?>
-<!--        <div class="p-5 --><?php //echo ( ! is_home() && ! is_front_page() ) ? 'bg-white' : 'h-full flex flex-col' ?><!--">-->
-<!--			--><?php //if ( ! is_home() && ! is_front_page() ): ?>
-<!--                <h3 class="text-xl font-semibold text-center mb-5">--><?php //echo get_the_title( $attached['survey'] ) ?><!--</h3>-->
-<!--                <div class="flex justify-center">-->
-<!--                    <div class="flex space-x-10 items-center mb-5 pb-5 border-b border-gray-800">-->
-<!--						--><?php //if ( has_post_thumbnail( $attached['survey'] ) ): ?>
-<!--                            <div class="rounded-full flex-none border border-primary-100 w-24 h-24">-->
-<!--								--><?php //echo get_the_post_thumbnail( $attached['survey'], 'thumbnail', [ 'class' => 'p-2 rounded-full' ] ); ?>
-<!--                            </div>-->
-<!--						--><?php //endif; ?>
-<!--                        <div>-->
-<!--                            <p>-->
-<!--								--><?php //echo get_the_excerpt( $attached['survey'] ) ?>
-<!--                            </p>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--			--><?php //endif; ?>
+		ob_start();
+		?>
+        <div class="p-5 <?php echo ( ! is_home() && ! is_front_page() ) ? 'bg-white' : 'h-full flex flex-col' ?>">
+			<?php if ( ! is_home() && ! is_front_page() ): ?>
+                <h3 class="text-xl font-semibold text-center mb-5"><?php echo get_the_title( $attached['survey'] ) ?></h3>
+                <div class="flex justify-center">
+                    <div class="flex space-x-10 items-center mb-5 pb-5 border-b border-gray-800">
+						<?php if ( has_post_thumbnail( $attached['survey'] ) ): ?>
+                            <div class="rounded-full flex-none border border-primary-100 w-24 h-24">
+								<?php echo get_the_post_thumbnail( $attached['survey'], 'thumbnail', [ 'class' => 'p-2 rounded-full' ] ); ?>
+                            </div>
+						<?php endif; ?>
+                        <div>
+                            <p>
+								<?php echo get_the_excerpt( $attached['survey'] ) ?>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+			<?php endif; ?>
 <!---->
 <!--                <questions-->
 <!--                        :questions="--><?php //echo htmlentities( json_encode( $survey_meta['questions'] ) ) ?><!--"-->
@@ -199,8 +199,8 @@ class FrontendQuestion {
 <!--				--><?php //endif; ?>
 <!--            </div>-->
 <!--        </div>-->
-<!--		--><?php
-//		$content .= ob_get_clean();
+		<?php
+		$content .= ob_get_clean();
 
 
 		return $content;
